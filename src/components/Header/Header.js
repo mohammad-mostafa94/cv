bf055ee201aacce7e3e3e059a5d2290a76841b33
mohammad-mostafa94 <mohammadmostafa1094@gmail.com>
2022-09-17 15:33:04 +0600

    header complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import mostafa from '../image/mostafa_pic.png';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <>
+            <div className='header'>
+                <div className="pic-name">
+                    <div className="pic">
+                        <img src={mostafa} alt="" />
+                    </div>
+                    <div className="nameInfo">
+                        <div className="name">
+                            <h2>Md.Golam Mostafa</h2>
+                            <h5>Software Developer(MERN)</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="linkInfo">
+                    <div className="links">
+                        <h4>Email:<a href='#'>gmshakilcse@gmail.com</a></h4>
+                        <h4>Location: <a href='#'>Dhaka,Bangladesh.</a></h4>
+                        <h4>Mobile:<a href='#'>+8801521320291</a></h4>
+                    </div>
+                </div>
+            </div>
+            <hr className="marginLine" />
+        </>
+    );
+};
+
+export default Header;
